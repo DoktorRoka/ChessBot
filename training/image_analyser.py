13b564e00ba51490982f3124a9b4c0b32696bfcb
roka <maximkalosha@gmail.com>
2024-02-08 23:15:20 +0300

    Add image analyser for chessboard piece detection
    This commit includes the addition of an image analyser using OpenCV to the training module. This image analyser scans through a chessboard image to detect pieces, specifically white and black pawns. This process involves loading images of the board and the pieces, making them grayscale, and setting bounding boxes to these images with specific dimensions. Later, using the matchTemplate function, the locations of the pawns are determined. Rectangular bounding boxes are then drawn around these located pieces.
    
    This automatic detection of pieces will help in processing training data of other skin packs. It's a step towards making the training data creator more efficient.

---
+++ training/image_analyser.py
@@ -0,0 +1,37 @@
+# TODO: I REALLY SHOULD JUST TRAIN MY MODEL IF I WANT IT TO DETECT PIECES FROM OTHER SKIN PACKS.
+
+import cv2
+import numpy as np
+
+# loading pics of board and pieces.
+# TODO: make it better
+img_board = cv2.imread('../train_data/pic_2.png')
+img_piece_pawn_white = cv2.imread('../train_data/white_pawn.png', cv2.IMREAD_UNCHANGED)
+img_piece_pawn_black = cv2.imread('../train_data/black_pawn.png', cv2.IMREAD_UNCHANGED)
+
+# making it gray scale, probably not needed because they have solid color. Need to change
+img_board_readable = cv2.cvtColor(img_board, cv2.COLOR_BGR2GRAY)
+img_piece_pawn_white_readable = cv2.cvtColor(img_piece_pawn_white, cv2.COLOR_BGR2GRAY)
+img_piece_pawn_black_readable = cv2.cvtColor(img_piece_pawn_black, cv2.COLOR_BGR2GRAY)
+
+# setting bound boxes to our pictures
+h_white, w_white = img_piece_pawn_white_readable.shape
+h_black, w_black = img_piece_pawn_black_readable.shape
+
+# searching for white pawn
+res_white = cv2.matchTemplate(img_board_readable, img_piece_pawn_white_readable, cv2.TM_CCOEFF_NORMED)
+threshold_white = 0.8
+loc_white = np.where(res_white >= threshold_white)
+
+for pt in zip(*loc_white[::-1]):
+    cv2.rectangle(img_board, pt, (pt[0] + w_white, pt[1] + h_white), (0, 0, 255), 2)
+
+# searching for black pawn
+res_black = cv2.matchTemplate(img_board_readable, img_piece_pawn_black_readable, cv2.TM_CCOEFF_NORMED)
+threshold_black = 0.8
+loc_black = np.where(res_black >= threshold_black)
+
+for pt in zip(*loc_black[::-1]):
+    cv2.rectangle(img_board, pt, (pt[0] + w_black, pt[1] + h_black), (255, 0, 0), 2)
+
+cv2.imwrite('res.png', img_board)
