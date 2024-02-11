bfc42f0ac6cf434aa0004d4872b2bae98fa5d10d
roka <maximkalosha@gmail.com>
2024-02-11 21:27:12 +0300

    Fully made FEN detector (working 99%)
    Made a fully working FEN detector, work chance is 99%. Sometimes, if figure skin pack is looking goofy, will detect rook as horse
    
    TODO:
    Need to add recognition for discord figures

--- training/image_analyser.py
+++ training/image_analyser.py
@@ -1,3 +1,3 @@
-# TODO: refactor start_analyse function from tensorflow_chessbot.py
+# TODO: refactor start_analyse function from fen_generator.py
 # TODO: remove chess_pieces from files because not needed anymore
 # TODO: include dataset as other file
