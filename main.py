8837d04ad84a541b63fa7b3acea0af655768237f
roka <maximkalosha@gmail.com>
2024-02-13 18:56:49 +0300

    Make it automatically move for you
    New: now it automatically moves pieces for you (currently working only on lichess, maybe will work if you change top_left_x, top_left_y and bottom_right_x, bottom_right_y vars)

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import sys
 sys.path.append("./training")
-
 from training.fen_generator import *
 from stockfish_init import ChessEngine
-
 import pyautogui
 import time
 
@@ -12,19 +10,34 @@ engine = ChessEngine("./stockfish/stockfish-windows-x86-64-avx2.exe")
 
 previous_fen = None
 
+# Define the coordinates of the chessboard's top-left and bottom-right corners
+top_left_x, top_left_y = 557, 220
+bottom_right_x, bottom_right_y = 1303, 964
+
+# Define the size of a square on the chessboard
+square_size = (bottom_right_x - top_left_x) / 8
+
 while True:
-    # Take a screenshot
-    screenshot = pyautogui.screenshot()
-    # Save the screenshot
+    # Take a screenshot of the chessboard
+    screenshot = pyautogui.screenshot(region=(top_left_x, top_left_y, bottom_right_x - top_left_x, bottom_right_y - top_left_y))
     screenshot.save("training/screenshot.png")
-    # Call start_detection with the screenshot file
     new_fen = start_detection(filepath="training/screenshot.png")
-    # If the FEN has changed, print a message
-    print(f'STOCKFISH CHECK {new_fen}')
-    if previous_fen != new_fen:
+    if previous_fen != new_fen:  # detects changed.
+        # TODO: make it to move only after one detection (to prevent CPU overload by stockfish)
         print("The FEN has changed.")
         best_move = engine.get_best_move(new_fen)
         print(best_move)
+
+        # Convert the move to screen coordinates
+        start_square, end_square = best_move[:2], best_move[2:]
+        start_x, start_y = top_left_x + square_size * (
+                    ord(start_square[0]) - ord('a')) + square_size / 2, top_left_y + square_size * (
+                                       8 - int(start_square[1])) + square_size / 2
+        end_x, end_y = top_left_x + square_size * (
+                    ord(end_square[0]) - ord('a')) + square_size / 2, top_left_y + square_size * (
+                                   8 - int(end_square[1])) + square_size / 2
+
+        pyautogui.moveTo(start_x, start_y)
+        pyautogui.dragTo(end_x, end_y, button='left')
         previous_fen = new_fen
-    # Wait for three seconds
-    time.sleep(3)
+    time.sleep(5)
